3cb2a9d4b048b7b0f68fdb7f6dd10f67bbd902ac
Seif_Eldeen <132578246+S-Eldeen@users.noreply.github.com>
2026-02-28 23:59:34 +0200

    initial python server

---
+++ app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type","text/plain")
+        self.end_headers()
+        self.wfile.write(b"Server is running")
+
+PORT = 5000
+
+print("Server Started...")
+server = HTTPServer(("0.0.0.0", PORT), handler)
+server.serve_forever()
